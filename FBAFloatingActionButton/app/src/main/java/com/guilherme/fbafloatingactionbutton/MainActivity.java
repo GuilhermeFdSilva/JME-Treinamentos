e0ef62643e5a5f280770584ad2738b2a8fd89565
Guilherme <francaguilherme27@gmail.com>
2023-09-09 16:03:41 -0300

    Floating Action Button

---
+++ FBAFloatingActionButton/app/src/main/java/com/guilherme/fbafloatingactionbutton/MainActivity.java
@@ -0,0 +1,35 @@
+package com.guilherme.fbafloatingactionbutton;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.guilherme.fbafloatingactionbutton.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener((view) -> {
+            Snackbar.make(view, "FAB pressionado", Snackbar.LENGTH_LONG)
+                    .setAction("Confirmar", null).show();
+        });
+    }
+}
