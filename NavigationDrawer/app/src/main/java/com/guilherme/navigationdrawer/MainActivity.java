7be4dd48eb781a03671e9b8cf22ef3c8e4df9f6f
Guilherme <francaguilherme27@gmail.com>
2023-09-14 13:38:28 -0300

    Entendendo a esturtura do projeto

---
+++ NavigationDrawer/app/src/main/java/com/guilherme/navigationdrawer/MainActivity.java
@@ -0,0 +1,58 @@
+package com.guilherme.navigationdrawer;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.guilherme.navigationdrawer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Usado para vincular as views
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Configura a barra superior
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        // Cria referencia para toda área de NavigationDrawer
+        DrawerLayout drawer = binding.drawerLayout;
+
+        // Cria referencia para área de navegação
+        NavigationView navigationView = binding.navView;
+
+        // Define configurações do NavigationDrawer
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+
+        // Configura a área que ira carregar os fragments
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        // Configura o menu superior de navegação
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        // Configura a navegação para o NavigationView
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
